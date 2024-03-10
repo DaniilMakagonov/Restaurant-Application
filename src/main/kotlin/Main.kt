10d0a4421ba119d6193c5e374fc89208d45fa06b
Daniil Makagonov <daniil1makagonov@gmail.com>
2024-03-10 22:55:17 +0300

    Message about errors in program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,8 @@ suspend fun main() {
         controller.start()
     } catch(e: Exception) {
         println(e.message)
-        println(e.stackTrace)
+        println(e.stackTrace.toString())
+        println("Send message with the description of error to developer:\ndamakagonov@edu.hse.ru")
     } finally {
         controller.exit()
     }
